a3b065809c615293c5a3c51232929c5752223248
Saakshig12 <saakshig12@gmail.com>
2019-08-21 21:10:48 -0400

    new files

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ app.use(logger("dev"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/unit18Populater", {useNewUrlParser: true})
+mongoose.connect("mongodb://localhost/cnn", {useNewUrlParser: true})
 
 app.get("/scrape", function (req, res) {
-    axios.get("link").then(function (res) {
+    axios.get("https://www.cnn.com/").then(function (res) {
         var $ = cheerio.load(res.data);
         $("article h2").each(function (i, element) {
             var result = {};
 
             result.title = $(this)
-            .children("a")
+            .children("h3")
             .text();
             result.link = $(this)
             .children("a")
@@ -50,3 +50,46 @@ app.get("/scrape", function (req, res) {
 
 // app.get()
 
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+    // TODO: Finish the route so it grabs all of the articles
+    db.Article.find({})
+      .then(function (dbArticle) {
+        res.json(dbArticle);
+      }).catch(function (err) {
+        res.json(err);
+      })
+  });
+  
+  // Route for grabbing a specific Article by id, populate it with it's note
+  app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note").then(function (dbArticle) {
+        res.json(dbArticle);
+      }).catch(function (err) {
+        res.json(err);
+      })
+    // TODO
+    // ====
+    // Finish the route so it finds one article using the req.params.id,
+    // and run the populate method with "note",
+    // then responds with the article with the note included
+  });
+  
+  // Route for saving/updating an Article's associated Note
+  app.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body).then(function (dbNote) {
+      return db.Article.findOneAndUpdate({}, { $push: { notes: dbNote._id } }, { new: true });
+    }).then(function (dbNote) {
+      res.json(dbNote);
+    }).catch(function (err) {
+      res.json(err);
+    })
+ 
+  });
+  
+  // Start the server
+  app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+  });
+  
