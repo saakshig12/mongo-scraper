1d0863535ca7a1a51588a879519fe0970184ff88
Saakshig12 <saakshig12@gmail.com>
2019-08-21 20:21:17 -0400

    html and server items added

---
+++ server.js
@@ -0,0 +1,10 @@
+//Adding all require packages
+
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
